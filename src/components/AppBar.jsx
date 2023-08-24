cc39788129e1ede64feea874b3ef1a8d20b18516
Rostyslav Haluzinskiy <106879174+RossHaluz@users.noreply.github.com>
2023-08-24 13:23:18 +0300

    add

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -6,7 +6,7 @@ const AppBar = () => {
 const {theme} = useSelector(state => state.auth)
 
   return (
-    <header className={`py-[18px] z-[999] px-[24px] flex justify-between ${theme === 'dark' && 'bg-[#161616]'} ${theme === 'violet' && 'bg-[#fff]'} ${theme === 'light' && 'bg-[#FCFCFC]'} lg:w-[calc(100vw-260px)] lg:fixed lg:top-0 lg:right-0 lg:flex`}>
+    <header className={`py-[18px] z-[999] px-[24px] flex justify-between ${theme === 'dark' && 'bg-[#161616]'} ${theme === 'violet' && 'bg-[#fff]'} ${theme === 'light' && 'bg-[#FCFCFC]'} fixed w-full lg:w-[calc(100vw-260px)] lg:top-0 lg:right-0 lg:flex`}>
       <Navigation/>
       <AuthNav/>
     </header>
